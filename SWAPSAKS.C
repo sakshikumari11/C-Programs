a830a9e6f7847df5fad62805f26a09d981e96685
sakshikumari11 <166231672+sakshikumari11@users.noreply.github.com>
2024-09-26 14:01:24 +0530

    Add files via upload

---
+++ SWAPSAKS.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10,b=20;
+	clrscr();
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("\n value of a = %d",a);
+	printf("\n value of b = %d",b);
+	getch();
+}
