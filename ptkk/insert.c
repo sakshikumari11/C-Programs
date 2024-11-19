8950b5f258ef70a6e63f6cc150d06182c74ebda2
sakshikumari11 <166231672+sakshikumari11@users.noreply.github.com>
2024-11-19 16:13:38 +0530

    Add files via upload

---
+++ ptkk/insert.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],i,j;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("Enter a[%d][%d]",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("a[%d][%d]=%d",i,j,a[i][j]);
+		}
+	}	
+	return 0;
+}
